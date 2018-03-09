90b9a9de469b0a6503b1fa4e3ef3658e860c121b
Alin <alingabriel.ciuca@gmail.com>
2018-03-09 07:59:58 +0200

    Add project files.

---
+++ bing-wallpaper-changer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bing_wallpaper_changer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
